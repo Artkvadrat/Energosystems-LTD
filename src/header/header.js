8f8676cfb8fa4daf55e75bf65bb99763f7d81b97
Artkvadrat <Artkvadrat984@gmail.com>
2020-03-02 23:09:06 +0200

    I have made a header block without adaptive and without changing of headers.

--- src/header/header.js
+++ src/header/header.js
@@ -18,49 +18,13 @@ export default class Header extends Component {
                        fluid={true}
                        style={ setBackground }>
                 <Row>
-                    <Col sm={0} md={1}>
-
-                    </Col>
-                    <Col md={10} className="d-flex justify-content-between">
+                    <Col md={12} className="d-flex justify-content-between">
                         <div className="logo d-flex">
                             <img src={logo} alt="Logo"/>
                             <p>Energo Systems LTD</p>
                         </div>
-                        <div className="d-flex">
-                            <div className="phones">
-                                <p className="phoneNumber">
-                                    <a href="tel:+380980325902">
-                                        +380(98)-032-59-02
-                                    </a>
-                                </p>
-                                <p className="phoneNumber">
-                                    <a href="tel:+380662338009">
-                                        +380(66)-233-80-09
-                                    </a>
-                                </p>
-                                <p className="phoneNumber">
-                                    <a href="tel:+380680684040">
-                                        +380(68)-068-40-40
-                                    </a>
-                                </p>
-                            </div>
-                            <div className="callBack">
-                                <button>
-                                    Обратная связь
-                                </button>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col md={1} sm={0}>
-
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={1} sm={0}>
-
-                    </Col>
-                    <Col md={10} sm={12} className="headerLinks">
-                        <ul className="headerLinksList">
+                        <div>
+                            <ul className="headerLinksList">
                             <li>
                                 <a href="">
                                     Услуги
@@ -92,15 +56,49 @@ export default class Header extends Component {
                                 </a>
                             </li>
                         </ul>
+                        </div>
+                        {/*<div>*/}
+                        {/*    <div className="phones">*/}
+                        {/*        <p className="phoneNumber">*/}
+                        {/*            <a href="tel:+380980325902">*/}
+                        {/*                +380(98)-032-59-02*/}
+                        {/*            </a>*/}
+                        {/*        </p>*/}
+                        {/*        <p className="phoneNumber">*/}
+                        {/*            <a href="tel:+380662338009">*/}
+                        {/*                +380(66)-233-80-09*/}
+                        {/*            </a>*/}
+                        {/*        </p>*/}
+                        {/*        <p className="phoneNumber">*/}
+                        {/*            <a href="tel:+380680684040">*/}
+                        {/*                +380(68)-068-40-40*/}
+                        {/*            </a>*/}
+                        {/*        </p>*/}
+                        {/*    </div>*/}
+                            <div className="callBack">
+                                <button>
+                                    Обратная связь
+                                </button>
+                            </div>
+                        {/*</div>*/}
+                    </Col>
+                </Row>
+
+                <div className="infoAboutPage">
+                    <p style={{color: 'white', fontSize: '70px', marginTop: '70px', letterSpacing: '3px'}}>МЫ СОЗДАЕМ КОМФОРТ</p>
+                    <p style={{color: 'white', fontSize: '40px', marginTop: '40px', paddingBottom: '90px'}}>а не просто устанавливаем отопление</p>
+                </div>
+                <Row>
+                    <Col md={1} sm={0}>
+
+                    </Col>
+                    <Col md={10} sm={12} className="headerLinks">
+
                     </Col>
                     <Col md={1} sm={0}>
 
                     </Col>
                 </Row>
-                <div className="infoAboutPage">
-                    <p style={{color: 'white', fontSize: '30px', marginTop: '40px'}}>Мы не просто устанавливаем отопление</p>
-                    <p style={{color: '#f13333', fontSize: '60px', marginTop: '40px', letterSpacing: '3px', paddingBottom: '50px'}}>Мы создаем комфорт</p>
-                </div>
             </Container>
         )
     }
